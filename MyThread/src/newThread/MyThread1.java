3fec34eb795883a565200b4fa0092918e11cf761
[git] <[745592658@qq.com]>
2017-07-17 17:21:14 +0800

    法力无边

--- MyThread/src/newThread/MyThread1.java
+++ MyThread/src/newThread/MyThread1.java
@@ -2,9 +2,9 @@
 
 public class MyThread1 extends Thread {
 	public void run(){
-         System.out.println("你好来自线程");
 	 for (int i = 0; i < 20; i++) {
 		System.out.println("你好来自线程:"+Thread.currentThread().getName());
 	}	
+       System.out.println("你好来自线程");
 	}
 }
