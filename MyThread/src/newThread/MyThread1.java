3790b33ae3f784e58fff5a04deb3ff4360648ddd
[git] <[745592658@qq.com]>
2017-07-17 19:17:36 +0800

    日月争辉

--- MyThread/src/newThread/MyThread1.java
+++ MyThread/src/newThread/MyThread1.java
@@ -2,6 +2,7 @@
 
 public class MyThread1 extends Thread {
 	public void run(){
+System.out.println("你好来自线程");
 	 for (int i = 0; i < 20; i++) {
 		System.out.println("你好来自线程:"+Thread.currentThread().getName());
 	}	
