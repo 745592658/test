fdc61741da7d0216bf0e0d0df420b1af2326030f
[git] <[745592658@qq.com]>
2017-07-17 15:08:42 +0800

    壮志饥餐胡虏肉

--- MyThread/src/newThread/MyThread1.java
+++ MyThread/src/newThread/MyThread1.java
@@ -1,7 +1,8 @@
-package newThread;
+﻿package newThread;
 
 public class MyThread1 extends Thread {
 	public void run(){
+         System.out.println("Thread 第一次提交");
 	 for (int i = 0; i < 20; i++) {
 		System.out.println("你好来自线程:"+Thread.currentThread().getName());
 	}	
